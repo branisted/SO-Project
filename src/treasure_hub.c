d73b760cef50ecc6dd6f6d600dbbe085cce19f99
Dragoș <branisted@gmail.com>
2025-05-06 16:39:54 +0300

    Working interactive CLI for treasure_hub

--- src/treasure_hub.c
+++ src/treasure_hub.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    printf("It works!\n");
+int main() {
+    char input[128];
+
+    printf("Treasure Hub:\n");
+
+    while (1) {
+        printf("\n> ");
+        fflush(stdout);
+
+        if (!fgets(input, sizeof(input), stdin))
+            break;
+
+        input[strcspn(input, "\n")] = '\0'; // stergere caracterul \n de la urma stringului de input
+
+        if (strcmp(input, "exit") == 0) {
+            {
+                remove("command.txt");
+                remove("result.txt");
+                break;
+            }
+
+        } else {
+            printf("Unknown command: %s\n", input);
+        }
+    }
+
     return 0;
 }
